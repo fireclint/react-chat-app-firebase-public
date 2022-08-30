d51c08cdbb470b8dac32f65cebca24942906c3a3
utkarshofficial <utkarshofficialin@gmail.com>
2022-08-31 02:16:16 +0530

    added style for chat box for scrolling
    this scrolling work on both side when new message is sent

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {auth} from '../firebase'
+import React from "react";
+import { auth } from "../firebase";
 
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
@@ -9,10 +9,10 @@ const style = {
 };
 
 const Message = ({ message }) => {
-  const messageClass = 
-  message.uid === auth.currentUser.uid
-  ? `${style.sent}`
-  : `${style.received}`
+  const messageClass =
+    message.uid === auth.currentUser.uid
+      ? `${style.sent}`
+      : `${style.received}`;
 
   return (
     <div>
--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import {auth, db} from '../firebase'
-import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const style = {
-  form: `h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0`,
+  form: `h-14 w-full max-w-[728px]  flex text-xl fixed bottom-0`,
   input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
   button: `w-[20%] bg-green-500`,
 };
 
-const SendMessage = ({scroll}) => {
-  const [input, setInput] = useState('');
+const SendMessage = ({ scroll }) => {
+  const [input, setInput] = useState("");
 
   const sendMessage = async (e) => {
-    e.preventDefault()
-    if (input === '') {
-        alert('Please enter a valid message')
-        return
+    e.preventDefault();
+    if (input === "") {
+      alert("Please enter a valid message");
+      return;
     }
-    const {uid, displayName} = auth.currentUser
-    await addDoc(collection(db, 'messages'), {
-        text: input,
-        name: displayName,
-        uid,
-        timestamp: serverTimestamp()
-    })
-    setInput('')
-    scroll.current.scrollIntoView({behavior: 'smooth'})
-  }
+    const { uid, displayName } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: serverTimestamp(),
+    });
+    setInput("");
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <form onSubmit={sendMessage} className={style.form}>
@@ -34,10 +34,10 @@ const SendMessage = ({scroll}) => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         className={style.input}
-        type='text'
-        placeholder='Message'
+        type="text"
+        placeholder="Message"
       />
-      <button className={style.button} type='submit'>
+      <button className={style.button} type="submit">
         Send
       </button>
     </form>
